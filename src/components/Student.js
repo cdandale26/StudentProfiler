16e668feaac681bf264160a568de840ab187686d
Chaitali D <cudandale@gmail.com>
2022-06-05 02:32:58 -0400

    css: Part2 to style the web page and replicate the style of the image and gif is completed in this commit

--- src/components/Student.js
+++ src/components/Student.js
@@ -7,27 +7,43 @@ const Student = (props) => {
       acc += parseInt(mark);
       return acc;
     }, 0);
-    return (totalMarks / marks.length).toFixed(2);
+    return totalMarks / marks.length;
+  };
+
+  const capitalizeText = (textStr) => {
+    return textStr.toUpperCase();
   };
 
   return (
     <>
       {props.studentData.map((student) => (
-        <div key={student.email}>
-          <div>
+        <div key={student.email} className="allInfo">
+          <div className="profileImage">
             <img src={student.pic} alt="" />
           </div>
-          <div className="StudentInfo">
+          <div className="studentInfo">
             <div>
-              <h3>
-                {student.firstName} {student.lastName}{" "}
-              </h3>
+              <span>
+                {capitalizeText(student.firstName)}{" "}
+                {capitalizeText(student.lastName)}{" "}
+              </span>
             </div>
-            <div>Email:{student.email}</div>
-            <div>Company:{student.company}</div>
-            <div>Skill:{student.skill}</div>
-            <div>Average:{findAverage(student.grades)} %</div>
+            <div className="sideInfo">
+              <div>Email: {student.email}</div>
+              <div>Company: {student.company}</div>
+              <div>Skill: {student.skill}</div>
+              <div>Average: {findAverage(student.grades)} %</div>
+              {/**<div>
+              {student.grades.map((grade, idx) => (
+                <div>
+                  Test{idx + 1} : {"  "}
+                  {grade}%
+                </div>
+              ))}
+              </div> */}
+            </div>
           </div>
+          <div className="viewGrades">+</div>
         </div>
       ))}
     </>
